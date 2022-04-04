509c0f219a388ffbd16aa712ad0b05c9106ea47e
Tezar Surya Fernanda <tzr.surya@gmail.com>
2022-04-04 23:34:31 +0700

    add nprogress and loader animation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,27 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+import '../styles/nprogress.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  NProgress.configure({
+    showSpinner: false,
+    speed: 500,
+    easing: 'ease',
+  })
+
+  useEffect(() => {
+    router.events.on('routeChangeStart', () => NProgress.start())
+    router.events.on('routeChangeComplete', () => NProgress.done())
+    router.events.on('routeChangeError', () => NProgress.done())
+  }, [])
+
   return (
     <ThirdwebProvider desiredChainId={ChainId.Rinkeby}>
       <Component {...pageProps} />
